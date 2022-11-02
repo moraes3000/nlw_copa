70434fe16782a8821c9716cc6b3d6354dda342c4
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-11-02 11:29:12 -0300

    layout SignIn

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { NativeBaseProvider, VStack, Center, Text } from "native-base";
+import { NativeBaseProvider, VStack, Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons'
 import Logo from '../assets/logo.svg'
+import { Button } from "../components/Button";
+
+
+
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900" >
-      <Logo />
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width='212' height='40' />
+      <Button
+        title='Entrar com o google'
+        type='SECONDARY'
+        mt={12}
+        leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
+      />
+      <Text color='white' textAlign='center' mt={4}>
+        Não utilizamos nenhuma informa além {'\n'} do seu e-mail para criação de sua conta.
+      </Text>
     </Center>
   )
 }
