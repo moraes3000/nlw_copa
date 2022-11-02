57d97df64d6514d6ebbdb8704aee9d522cc282b9
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-11-02 13:10:56 -0300

    autenticação  signInWithGoogle

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,8 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
 
+WebBrowser.maybeCompleteAuthSession()
 export interface AuthContextDataProps {
   user: UserProps;
-  signIn: () => Promise<void>
+  signIn: () => Promise<void>;
+  isUserLoading: boolean
 }
 
 interface UserProps {
@@ -17,17 +22,43 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps)
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '1075115750085-s523snaindtng815eua0dbb1vvodrai0.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  })
+
   async function signIn() {
-    console.log("vamos logar")
+    try {
+      setIsUserLoading(true)
+      await promptAsync()
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+    finally {
+      setIsUserLoading(false)
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log("Token de autenticação =====> ", access_token)
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
 
   return (
     <AuthContext.Provider value={{
       signIn,
-      user: {
-        name: "bruno",
-        avatarUrl: 'https://avatars.githubusercontent.com/u/29808643?v=4'
-      }
+      isUserLoading,
+      user
     }}>
       {children}
     </AuthContext.Provider>
