33e5119d256c391a580b1557a4f5646b0d31fa11
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-11-03 11:33:11 -0300

    detail pool

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -57,7 +57,7 @@ export async function poolRoutes(fastify: FastifyInstance) {
     return reply.status(201).send({ code })
   })
 
-  fastify.post('/pools/:id/join', { onRequest: [authenticate] }, async (request, reply) => {
+  fastify.post('/pools/join', { onRequest: [authenticate] }, async (request, reply) => {
     const joinPoolBody = z.object({
       code: z.string()
     })
@@ -147,6 +147,45 @@ export async function poolRoutes(fastify: FastifyInstance) {
 
     return { pools }
   })
+
+  fastify.get('/pools/:id', { onRequest: [authenticate] }, async (request) => {
+    const getPoolParams = z.object({
+      id: z.string()
+    })
+
+    const { id } = getPoolParams.parse(request.params)
+
+
+    const pools = await prisma.pool.findUnique({
+      where: {
+        id
+      },
+      include: {
+        owner: true,
+        _count: {
+          select: {
+            participants: true
+          }
+        },
+        participants: {
+          select: {
+            id: true,
+            user: {
+              select: {
+                avatarUrl: true,
+                nome: true
+              }
+            }
+          },
+          take: 4
+        }
+
+      }
+    })
+
+    return { pools }
+  })
+
 
 }
 
