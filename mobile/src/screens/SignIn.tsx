84ba98eebb6d49b8b7c51ee7392e47f2025b0117
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-11-03 14:50:34 -0300

    login mobile

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -9,7 +9,7 @@ import { useAuth } from "../hooks/useAuth";
 
 
 export function SignIn() {
-  const { signIn } = useAuth()
+  const { signIn, isUserLoading } = useAuth()
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width='212' height='40' />
@@ -19,6 +19,8 @@ export function SignIn() {
         mt={12}
         leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
         onPress={signIn}
+        isLoading={isUserLoading}
+        _loading={{ _spinner: { color: 'white' } }}
       />
       <Text color='white' textAlign='center' mt={4}>
         Não utilizamos nenhuma informa além {'\n'} do seu e-mail para criação de sua conta.
