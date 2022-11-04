ac7ac8479f3aecf7e2d7328f90be7e480bb9bd3b
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-11-04 12:15:24 -0300

    pools

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,13 +1,51 @@
-import { VStack, Icon } from "native-base";
+import { useCallback, useState } from "react";
+import { api } from "../services/axios";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 
 import { Octicons } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { PoolCard, PoolCardPros } from "../components/PoolCard";
+import { Loading } from "../components/Loading";
+import { EmptyPoolList } from "../components/EmptyPoolList";
 
 export function Pools() {
+  const [isLoading, setLoading] = useState(true)
+  const [pools, setPools] = useState<PoolCardPros[]>([])
+
+  const toast = useToast()
+
   const { navigate } = useNavigation();
+
+  async function featPools() {
+    try {
+      setLoading(true)
+      const response = await api.get('/pools')
+      setPools(response.data.pools)
+      // console.log(response.data.pools)
+
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        title: "Error, ao carregar bolões",
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      featPools()
+    }, [])
+  )
+
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -28,6 +66,22 @@ export function Pools() {
           onPress={() => navigate('find')}
         />
       </VStack>
+
+      {isLoading ?
+        <Loading />
+        : (
+          <FlatList
+            data={pools}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => <PoolCard data={item} />}
+            ListEmptyComponent={() => <EmptyPoolList />}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{ pb: 10 }}
+            px={5}
+          />
+        )
+      }
+
     </VStack>
   )
 }
