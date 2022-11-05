7ab26bbf3c6e01303dc1577e59a01af1d055ea49
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-11-05 12:30:12 -0300

    list detail

--- mobile/src/screens/Detail.tsx
+++ mobile/src/screens/Detail.tsx
@@ -1,7 +1,10 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { VStack, Heading, Text, useToast } from "native-base";
 import React, { useEffect, useState } from "react";
+import { EmptyMyPoolList } from "../components/EmptyMyPoolList";
 import { Header } from "../components/Header";
+import { PoolCardPros } from "../components/PoolCard";
+import { PoolHeader } from "../components/PoolHeader";
 import { api } from "../services/axios";
 
 interface RouteParams {
@@ -13,20 +16,21 @@ export function Detail() {
   const { id } = route.params as RouteParams
 
 
-  const [detail, setDetail] = useState({})
+  const [details, setDetail] = useState<PoolCardPros>({} as PoolCardPros)
   const [isLoading, setIsLoading] = useState(false)
 
   const toast = useToast()
 
   const { navigate } = useNavigation()
 
-  async function handleJoinPool() {
+  async function fetchPoolsDetails() {
 
 
     try {
       setIsLoading(true)
-      const response = await api.get(`/pools/${id}/games`)
-      console.log(response.data)
+      const response = await api.get(`/pools/${id}`)
+      setDetail(response.data.pools)
+      // console.log(response.data.pools)
 
     } catch (error) {
       console.log(error)
@@ -44,13 +48,21 @@ export function Detail() {
   }
 
   useEffect(() => {
-    handleJoinPool()
+    fetchPoolsDetails()
   }, [])
 
 
   return (
     <VStack flex={1} bgColor="gray.900">
-      <Header title={`Detail - ${id}`} showBackButton />
+      <Header title={`---Detail -  ${id}  `} showBackButton />
+
+      <VStack px={5} flex={1}>
+        {details._count?.participants > 0 ?
+          <PoolHeader data={details} /> :
+          <EmptyMyPoolList code={details.code} />
+        }
+      </VStack>
+
     </VStack>
   )
 }
